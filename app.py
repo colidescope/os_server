77a5dee8ab2da990f563fe499291acee20134b1e
Danil Nagy <4085524+danilnagy@users.noreply.github.com>
2024-12-17 10:45:20 -0500

    implment object selection

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI, HTTPException, Form
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
-from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeSphere
 from OCC.Core.BRepGProp import brepgprop_LinearProperties, brepgprop_SurfaceProperties, brepgprop_VolumeProperties
 from OCC.Core.GProp import GProp_GProps
 
 from OCC.Extend.DataExchange import write_iges_file
 
-import os
+import os, json
 import boto3
 from uuid import uuid4
 
@@ -21,10 +21,9 @@ if ENV != "PROD":
 
 app = FastAPI()
 
-class CreateBoxBody(BaseModel):
-    x: float
-    y: float
-    z: float
+class CreateObjectBody(BaseModel):
+    product: str
+    params: str
 
 # Fetch S3 configuration from environment variables
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
@@ -45,57 +44,37 @@ s3_client = boto3.client(
     region_name=S3_REGION
 )
 
-def get_box_statistics(box_shape):
-    """
-    Calculate statistics for the given box shape.
-    """
-    props = GProp_GProps()
-    
-    # Volume
-    brepgprop_VolumeProperties(box_shape, props)
-    volume = props.Mass()
+def get_cube(params):
+    print("Generating cube with params:", params["x"], params["y"], params["z"])
+    return BRepPrimAPI_MakeBox(params["x"], params["y"], params["z"]).Shape()
 
-    # Surface area
-    brepgprop_SurfaceProperties(box_shape, props)
-    surface_area = props.Mass()
+def get_sphere(params):
+    print("Generating sphere with params:", params["r"])
+    return BRepPrimAPI_MakeSphere(params["r"]).Shape()
 
-    # Bounding box dimensions
-    brepgprop_LinearProperties(box_shape, props)
-    length = props.CentreOfMass().X() * 2
-    width = props.CentreOfMass().Y() * 2
-    height = props.CentreOfMass().Z() * 2
-    
-    return {
-        "volume": volume,
-        "surface_area": surface_area,
-        "length": length,
-        "width": width,
-        "height": height,
-    }
+function_map = {
+    "cube": get_cube,
+    "sphere": get_sphere
+}
 
-@app.get("/")
-def read_root():
-    my_box = BRepPrimAPI_MakeBox(20.0, 20.0, 20.0).Shape()
-    box_stats = get_box_statistics(my_box)
-    
-    return {
-        "status": "success",
-        "box_statistics": box_stats,
-    }
+@app.get("/items")
+def get_items():
+    return list(function_map)
 
-@app.post("/create-box")
-async def create_box(box_data: CreateBoxBody):
+@app.post("/create-object")
+async def create_object(object_data: CreateObjectBody):
     
-    x = box_data.x
-    y = box_data.y
-    z = box_data.z
+    product = object_data.product
+    params = object_data.params
+
+    print(product, params)
 
     try:
-        my_box = BRepPrimAPI_MakeBox(x, y, z).Shape()
-    
+        geometry = function_map[product](json.loads(params))
+
         # Save IGES file locally
         local_iges_filename = "{}.iges".format(uuid4())
-        write_iges_file(my_box, local_iges_filename)
+        write_iges_file(geometry, local_iges_filename)
 
         s3_key = "iges_files/{}".format(local_iges_filename)
         with open(local_iges_filename, "rb") as file:
@@ -112,7 +91,7 @@ async def create_box(box_data: CreateBoxBody):
         }
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to create the box BREP data: {e}")
+        raise HTTPException(status_code=500, detail=f"Failed to create the BREP data: {e}")
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
